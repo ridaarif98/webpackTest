2801e72bf394a08ea02e1bc4192a3e23439a5bf0
ridaarif98 <ridaarif16@gmail.com>
2021-07-19 18:36:59 +0200

    add background color

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
+     
       index: './src/index.js',
-      print: './src/print.js',
     },
    plugins: [
      new HtmlWebpackPlugin({
-       title: 'Output Management',
-       template: './src/index.html',
+     title: 'Output Management',
      }),
    ],
     output: {
@@ -17,6 +17,14 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       clean: true,
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
   };
 
 // module.exports = {
